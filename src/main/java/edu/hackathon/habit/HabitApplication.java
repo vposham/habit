b0aaec103c7bf43431116dd0c7d21a419c288666
vposhamod <vignan.posham@officedepot.com>
2019-10-05 20:08:12 -0400

    CORS fix

--- src/main/java/edu/hackathon/habit/HabitApplication.java
+++ src/main/java/edu/hackathon/habit/HabitApplication.java
@@ -2,6 +2,10 @@ package edu.hackathon.habit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HabitApplication {
@@ -10,4 +14,13 @@ public class HabitApplication {
         SpringApplication.run(HabitApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
